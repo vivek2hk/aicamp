3c5fde71a4e245fe4ea28a4f0b17054e82af06b2
“supriyakr” <supriyakr222@gmail.com>
2024-06-01 10:02:53 -0700

    basic rag

---
+++ load_climate_data.py
@@ -0,0 +1,38 @@
+import pymongo
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_community.llms import OpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+import os
+config = load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+openaiClient = OpenAI()
+import certifi
+
+# set up a MongoDB Atlas vector store
+mongoClient = pymongo.MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+dbName= "rag_climate_demo"
+collectionName = "climate_data"
+collection= mongoClient[dbName][collectionName]
+
+loader = DirectoryLoader('./data_files',glob='*.txt',show_progress=True)
+data = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+docs = text_splitter.split_documents(data)
+
+
+vector_store = MongoDBAtlasVectorSearch.from_documents(
+    documents=docs,
+    embedding=OpenAIEmbeddings(disallowed_special=()),
+    collection=collection,
+    index_name="climate_search_index",
+    
+)
+
+# create a retrieval QA chain 
+
--- load_data_airbnb.py
+++ load_data_airbnb.py
@@ -21,9 +21,9 @@ def generate_embedding(text: str) -> list[float]:
 
     return openaiClient.embeddings.create(model="text-embedding-3-small",input=text).data[0].embedding
    
-# To create embeddings for all documents in the collection, here the limit is set to 50
-# for doc in collection.find({'summary':{"$exists": True}}).limit(50):
-#   doc['summary_embedding_openai'] = generate_embedding(doc['summary'])
+#To create embeddings for all documents in the collection
+# for doc in collection.find({'summary':{"$exists": True}}):
+#   doc['summary_embedding_oi'] = generate_embedding(doc['summary'])
 #   collection.replace_one({'_id': doc['_id']}, doc)
 
 
@@ -31,12 +31,12 @@ query = "I am looking for a place to stay in near a National Park. I would like 
 results = collection.aggregate([
   {"$vectorSearch": {
     "queryVector": generate_embedding(query),
-    "path": "summary_embedding_openai",
+    "path": "summary_embedding_oi",
     "numCandidates": 100,
     "limit": 4,
-    "index": "vector_index",
+    "index": "airbnb_search_index",
       }}
 ]);
 
 for document in results:
-    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
+    print(f'Airbnb Name: {document["name"]},\nLocation Summary: {document["summary"]}\n')
